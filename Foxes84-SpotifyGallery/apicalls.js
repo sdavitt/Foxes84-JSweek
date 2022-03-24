f0a924bb404ae1ca8f925509b52f1341f5a450b2
sdavitt <sdavt2@gmail.com>
2022-03-24 12:42:39 -0500

    spotify poster start

---
+++ Foxes84-SpotifyGallery/apicalls.js
@@ -0,0 +1,29 @@
+// Function for getting our access token
+const getAuth = async () => {
+    //client id/secret here as variables
+    const response = await fetch('https://accounts.spotify.com/api/token', 
+        {method: 'POST',
+        headers: {'Authorization': `Basic ${btoa(clientID+':'+clientSecret)}`,
+                'Content-Type': 'application/x-www-form-urlencoded'},
+        body: 'grant_type=client_credentials'});
+    const token = await response.json();
+    console.log(token);
+    return token.access_token
+}
+
+
+
+const getSong = async (songname, artist) => {
+    const token = await getAuth();
+    let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=track:${songname}+artist:${artist}`, 
+        {method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }});
+    console.log(response);
+    let data = await response.json();
+    console.log(data.tracks.items[0]);
+}
+
+getSong('Coffee', 'Sylvan Esso');
